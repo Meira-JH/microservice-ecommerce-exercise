01537e56b3545025da95bb80b6871835f1570d16
Meira-JH <jhalvesmeira@gmail.com>
2024-03-03 11:56:22 -0300

    creates eureka-server folder

--- eureka-server/src/main/java/com/store/eurek/EurekApplication.java
+++ eureka-server/src/main/java/com/store/eurek/EurekApplication.java
@@ -2,8 +2,10 @@ package com.store.eurek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekApplication {
 
 	public static void main(String[] args) {
