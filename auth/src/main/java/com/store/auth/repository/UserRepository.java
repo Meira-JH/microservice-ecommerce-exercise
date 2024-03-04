b32ce4f0aa0e3e83c663fe73fb32bca17cdef8b1
Meira-JH <jhalvesmeira@gmail.com>
2024-03-04 18:00:37 -0300

    repositories and generic service

---
+++ auth/src/main/java/com/store/auth/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.store.auth.repository;
+import com.store.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
