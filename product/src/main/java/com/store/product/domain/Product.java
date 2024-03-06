334b6f12192c609050290ea9227f72bc2f38a8f3
Meira-JH <jhalvesmeira@gmail.com>
2024-03-06 09:03:00 -0300

    product domain and necessary plugin installationZ

---
+++ product/src/main/java/com/store/product/domain/Product.java
@@ -0,0 +1,27 @@
+package com.store.product.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import lombok.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
---
+++ product/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name) VALUES ('Produto 1');
+INSERT INTO product (name) VALUES ('Produto 2');
+INSERT INTO product (name) VALUES ('Produto 3');
